f934382628511898062c121fbca6d870718f7f2a
Lukas Stuppacher <magicluks@live.at>
2024-07-16 15:28:38 +0200

    v.0.10.0 - U5

---
+++ api/src/models/DeckContentModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace deckbuilder_archive_php_version_only\api\models;
+class DeckContentModel
+{
+    public $cardName;
+    public $cardId;    
+    public $cardType;
+    public $subType;
+    public $superType;
+    public $manaValue;
+    public $castingCost;
+    public $quantity;
+}
--- api/src/services/DeckBuilderService.php
+++ api/src/services/DeckBuilderService.php
@@ -13,16 +13,72 @@ class DeckBuilderService
             DBPasword
         );
     }
+    public function addCardToMainboard($cardId, $sideBoard, $maybeBoard) {
+        $quantity = 1;
+        $this->addCardToSession($_SESSION['mainboard'], $cardId, $quantity);
+    }
+    
+    public function addCardToSideboard($cardId, $sideBoard, $maybeBoard) {
+        $quantity = 1;
+        $this->addCardToSession($_SESSION['side'], $cardId, $quantity);
+    }
+    
+    public function addCardToMaybeboard($cardId, $sideBoard, $maybeBoard) {
+        $quantity = 1;
+        $this->addCardToSession($_SESSION['maybe'], $cardId, $quantity);
+    }
+    private function addCardToSession(&$sessionArray, $cardId, $quantity) {
+        if (array_key_exists($cardId, $sessionArray)) {
+            $sessionArray[$cardId]['quantity'] += $quantity;
+        } else {
+            $sessionArray[$cardId]['quantity'] = $quantity;
+        }
+    } 
+    public function removeCardFromMainboard($cardId, $quantity) {
+        $_SESSION['mainboard'][$cardId]['quantity'] -= $quantity;
+        if($_SESSION['mainboard'][$cardId]['quantity'] <= 0){
+            unset($_SESSION['mainboard'][$cardId]);
+        }
+    }
+    public function removeCardFromSideboard($cardId, $quantity) {
+        $_SESSION['side'][$cardId]['quantity'] -= $quantity;
+        if($_SESSION['side'][$cardId]['quantity'] <= 0){
+            unset($_SESSION['side'][$cardId]);
+        }
+    }
+    public function removeCardFromMaybeboard($cardId, $quantity) {
+        $_SESSION['maybe'][$cardId]['quantity'] -= $quantity;
+        if($_SESSION['maybe'][$cardId]['quantity'] <= 0){
+            unset($_SESSION['maybe'][$cardId]);
+        }    
+    }
     public function createDecklist($userId, $deckName, $format){
         $newDecklist = $this->deckBuilderGateway->createDecklist($userId, $deckName, $format);
         return $newDecklist;
     }
     public function deleteDecklistEntry($deckId){
         if($deckId){
+            $this->deckBuilderGateway->removeDeckContentsFromDB($deckId);
             $this->deckBuilderGateway->deleteDecklistEntry($deckId);
             return true;
         }else{
             return false;
         }
+    }
+    public function removeDeckContents($deckId){
+        $this->deckBuilderGateway->removeDeckContentsFromDB($deckId);
+        return true;
+    }
+    public function uploadMainDeckContents($cardId,$deckId,$quantity){
+        $this->deckBuilderGateway->pushMainDeckContentIntoDB($cardId,$deckId,$quantity);
+        return true;        
+    }
+    public function uploadSideDeckContents($cardId,$deckId,$quantity){
+        $this->deckBuilderGateway->pushSideDeckContentIntoDB($cardId,$deckId,$quantity);
+        return true;        
+    }
+    public function uploadMaybeDeckContents($cardId,$deckId,$quantity){
+        $this->deckBuilderGateway->pushMaybeDeckContentIntoDB($cardId,$deckId,$quantity);
+        return true;        
     }
 }
